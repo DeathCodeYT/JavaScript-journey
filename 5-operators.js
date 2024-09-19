29e1e1e48241252583292a6aef83eb4211d5f6b1
DeathCodeYT <dc.deathcode@gmail.com>
2024-09-19 11:26:33 +0530

    Operators

---
+++ 5-operators.js
@@ -0,0 +1,41 @@
+// Arithmetic Operators (+,-,*,/,%,++,--,**)
+
+let rem = 47%3
+let num = 4;
+
+// console.log(num++)
+// console.log(num)
+console.log(5**4)
+
+// console.log(rem)
+// Assignments Operators (=,+=,-=,*=,/=)
+
+let bi = 32
+bi -= 16 // bi = bi-16
+bi *= 2
+bi /= 2
+let a = b = c = 66
+
+// console.log([a,b,c])
+
+// Comparision Operators (==,<,>,<=,>=,===,!==,!=)
+
+// console.log(2==2) / true
+// console.log(2==4) / false
+// console.log(2>2)
+// console.log(2>=8)
+// console.log(2!=2)
+// console.log('2'===2)
+
+// console.log(null >= 1)
+// console.log(undefined >= 1)
+
+// Logical Operators (&&,||,!,??)
+// console.log(2==2 && 1==1)
+// console.log(2==4 || 1==0)
+// console.log(!(2<7))
+
+let score = 9;
+let newScore = score ?? 0;
+
+// console.log(newScore)
