1d97499f76db6a6b442b4b24d1795527a28baf66
DeathCodeYT <dc.deathcode@gmail.com>
2024-09-21 11:30:58 +0530

    String and String Functions/Methods

---
+++ 7-string.js
@@ -0,0 +1,20 @@
+let str = 'DeathCode'
+let str2 = new String("DeathCode")
+
+// console.log(str.__proto__)
+// console.log(str[7])
+
+let username = "      username,email       "
+username = username.trim()
+// console.log(username.toUpperCase())
+// console.log(username.indexOf('n'))
+// console.log(username.charAt(4))
+// console.log(username.replace("@#","$"))
+// console.log(username.replaceAll("@","$"))
+
+// console.log(username.slice(1,6))
+console.log(username.split(""))
+
+
+
+
