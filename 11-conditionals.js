e9969b7700a0c76b9eab3ff050302eacd39035d2
DeathCodeYT <dc.deathcode@gmail.com>
2024-10-31 11:29:33 +0530

    Contionals Programming: if-else/switch

---
+++ 11-conditionals.js
@@ -0,0 +1,79 @@
+
+// let age = 17
+
+// if(age > 28){
+//     // code to execute if condition is true
+//     console.log("Age is greater then 28");
+// }else if (age >= 18) {
+//     console.log("You are now Adult");
+// }else{
+//     console.log("You are now Minor");
+// }
+
+let isLogin =  true;
+
+// if(isLogin){
+//     console.log("user is logged in")
+// }else{
+//     console.log("user is not logged in")
+// }
+
+// if (isLogin) 
+//     console.log("user is logged in"),console.log("hello");
+// else 
+//     console.log("user is not logged in");
+
+// if("Hello"=="Hello" &&  "World"=="World" ){
+
+//     console.log("Strings are equal");
+// }else{
+//     console.log("Strings are not equal");
+// }
+
+let token =  "";
+
+// if(token){
+//     console.log("token is valid");
+// }else{
+//     console.log("token is invalid");
+// }
+
+// Falsy Values: false, 0, -0, BigInt 0n, undefiend, null, NaN, "", 0.00
+// Truthy Values: true,"0", " ", function(){}, [], {},  1, 0.1, Infinity, "false", "null"
+
+let users = []
+
+// if(users.length != 0 ){
+//     console.log("users array is not empty");
+// }else{
+//     console.log("users array is empty");
+// }
+
+let week = "fri"
+// switch(week){
+//     case "mon":
+//         console.log("Monday");
+//         break
+//     case "tue":
+//         console.log("Tuesday");
+//         break
+//     case "wed":
+//         console.log("Wednesday");
+//         break
+//     case "fri":
+//         console.log("Friday");
+//         break
+//     default:
+//         console.log("Invalid day");
+// }
+
+// console.log("this is outside of if");
+
+
+// Terniary Operator
+
+// condition ? true : false
+isLogin =  false
+
+isLogin ? console.log("user is logged in") : console.log("user is not logged in")
+
