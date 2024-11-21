cbd5f12e3ae2b640d4b2dfade2470a2e9b0b389a
DeathCodeYT <dc.deathcode@gmail.com>
2024-11-21 10:55:29 +0530

    Async and Await Work

---
+++ 19-async-await.js
@@ -0,0 +1,12 @@
+
+console.log(1)
+setTimeout(()=>{console.log("2")},2000)
+
+console.log(3)
+console.log(4)
+
+// async function fun1() {
+//     // alskdjfa;lskdf
+//     // asdflkajsd;flkjasdfl;jk
+//     // await fetch()
+// }
